e17c95d447acc19ec35e21fb204f83980952da27
Arkavo Hait <arkavohait@gmail.com>
2021-07-23 04:46:09 +0530

    e_field

--- src/E_field.py
+++ src/E_field.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numba
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from charge_core import*
 from constants import*
@@ -7,3 +9,7 @@ from constants import*
 class E_field:
     def __init__(self,R):        
         self = np.zeros([R],dtype=float)
+
+    def display_f(self):
+        sns.heatmap(self)
+        plt.show()
